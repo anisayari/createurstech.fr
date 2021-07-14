4a1495c382d798442ab58a7b85fc41af7bbe361b
Guillaume <guillaume.kusberg@gmail.com>
2021-07-14 22:33:07 +0200

    add back history to search + focus on changed field

--- assets/js/filterAndSearch.js
+++ assets/js/filterAndSearch.js
@@ -4,69 +4,81 @@ var $grid = $(".Grid").isotope({
   layoutMode: "masonry",
   masonry: {
     columnWidth: ".Grid__sizer"
-  },
-});
-$grid.isotope('shuffle');
+  }
+})
+$grid.isotope("shuffle")
 
-const params = new URLSearchParams(window.location.search)
+let params = new URLSearchParams(window.location.search)
 const searchForm = document.querySelector("form")
 const searchNameInput = document.querySelector("input[name=\"creator_name\"]")
 const formElements = searchForm.elements
 
-const creators = document.querySelectorAll('.Card')
+const creators = document.querySelectorAll(".Card")
 
 const init = () => {
   Array.from(formElements).map(element => {
-    element.getAttribute("value") === params.get("platform") ? element.setAttribute("checked", "true") : ""
-    element.getAttribute("value") === params.get("category") ? element.setAttribute("checked", "true") : ""
+    if (element.getAttribute("value") === params.get("platform") && !element.checked) {
+      element.checked = true
+      element.focus()
+    }
+    if (element.getAttribute("value") === params.get("category") && !element.checked) {
+      element.checked = true
+      element.focus()
+    }
     if (element.getAttribute("name") === "creator_name" && null !== params.get("creator_name")) {
+      const oldValue = element.getAttribute("value")
       searchNameInput.setAttribute("value", params.get("creator_name"))
+      oldValue !== params.get("creator_name") && element.focus()
     }
   })
 
-  filterCreators();
+  filterCreators()
 }
 
 const filterCreators = () => {
   Array.from(creators).map(creator => {
-    const creatorName = creator.querySelector('.CreatorName')
+    const creatorName = creator.querySelector(".CreatorName")
 
     if (
       params.get("platform") && !creator.classList.contains(params.get("platform"))
-      || params.get("category") && !creator.classList.contains(decodeURIComponent(params.get("category").toLowerCase()).split(' ').join('_'))
+      || params.get("category") && !creator.classList.contains(decodeURIComponent(params.get("category").toLowerCase()).split(" ").join("_"))
       || params.get("creator_name") && !creatorName.innerHTML.toString().toLowerCase().includes(params.get("creator_name").toLowerCase())
     ) {
-      creator.style.display = "none";
+      creator.style.display = "none"
     } else {
-      creator.style.display = "grid";
+      creator.style.display = "grid"
     }
   })
 
-  refreshCards();
+  refreshCards()
 }
 
-init();
+init()
 
 const updateParams = () => {
   if (params.get("creator_name")) {
-    searchNameInput.setAttribute("value", params.get("creator_name"));
+    searchNameInput.setAttribute("value", params.get("creator_name"))
   }
 
-  const data = new FormData(searchForm);
+  const data = new FormData(searchForm)
 
   for (let [name, value] of data) {
-    params.set(name, encodeURIComponent(value));
+    params.set(name, encodeURIComponent(value))
   }
 
-  window.history.replaceState(
+  window.history.pushState(
     {},
     "",
     decodeURIComponent(`${window.location.pathname}?${params}`)
-  );
+  )
 
-  filterCreators();
+  filterCreators()
 }
 
-searchNameInput.addEventListener("oninput", updateParams);
-searchForm.addEventListener("change", updateParams);
-searchForm.addEventListener("submit", () => false);
+window.addEventListener("popstate", () => {
+  params = new URLSearchParams(window.location.search)
+  init()
+})
+searchNameInput.addEventListener("oninput", updateParams)
+searchForm.addEventListener("change", updateParams)
+searchForm.addEventListener("submit", () => false)
